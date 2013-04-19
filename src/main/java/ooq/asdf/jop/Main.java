9b3036436d5f91b01d89a352383ab0e9719c799c
methylene <theairspeedindicator@yahoo.com>
2013-04-19 03:16:46 -0400

    starting over with JOptionPane

---
+++ src/main/java/ooq/asdf/jop/Main.java
@@ -0,0 +1,11 @@
+package ooq.asdf.jop;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+	
+	public static void main(final String[] args) {
+		final String privKey = JOptionPane.showInputDialog("Enter private key: ");
+	}
+
+}
